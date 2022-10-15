cb72a15091e84f136e352b2c208621470c8cd9bc
nathanaelyou <nathanaelyou@gmail.com>
2022-10-15 08:18:25 -0400

    Step 4 finished

--- envoto.py
+++ envoto.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,25 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/showSignUp')
-def showSignUp():
-   return render_template('signup.html')
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/api/signup', methods=['POST'])
+def signUp():
+    # create user code will be here !!
+
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html': '<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
 
 
 if __name__ == '__main__':
