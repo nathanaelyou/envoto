8c23baa17f96575e778d8be5ba939440423c0660
nathanaelyou <nathanaelyou@gmail.com>
2022-10-15 02:05:36 -0400

    Working

--- envoto.py
+++ envoto.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'hello test'
-if __name__ == '__main__':
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
     app.run()
